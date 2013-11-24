4f990b769ad5d4d71faa6bd73631fd9218856f79
Vishnu Prathish <vishnu.prathish@gmail.com>
2013-11-24 13:00:03 -0500

    Coverts data format into adjecency list

---
+++ convpy.py
@@ -0,0 +1,34 @@
+#convpy.py 
+#vish
+#    Source format
+#	20 Berlin Frankfurt
+#	50 Berlin Munich
+#	20 Frankfurt Berlin
+#	10 Frankfurt Munich
+#	 
+#
+#    Destination Format:
+#
+#    	Berlin\tFrankfurt:20\tMunich:50
+#    	Frankfurt\tBerlin:20\tMunich:10
+#    	Munich
+
+
+
+file = open('test1.dat');
+
+Dics=dict()
+words=list()
+
+for line in file:
+    print line
+    words=line.split()
+    if Dics.get(words[1])==None:
+        Dics[words[1]]=words[1]+"\t"+str(words[2])+":"+str(words[0])
+    else:
+        Dics[words[1]]=Dics[words[1]]+"\t"+str(words[2])+":"+str(words[0])
+    Dics[words[2]]=words[2]
+
+    
+for line in Dics:
+    print Dics[line]
